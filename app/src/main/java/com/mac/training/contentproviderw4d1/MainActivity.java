c8e547c691e1dd60b428eca616b2e7bca4e60e92
NuccioJohn <nuccio.john@gmail.com>
2016-07-18 10:59:54 -0400

    First Commit

---
+++ app/src/main/java/com/mac/training/contentproviderw4d1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mac.training.contentproviderw4d1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
